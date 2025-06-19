5ce084d782bbb6b240812bb3a70449554dc5e030
Arthur Phillip Silva <artphil.dev@gmail.com>
2025-06-18 21:58:54 -0300

    create code visualization

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
+let codePreview = false;
+
+const html = document.querySelector(".main_content");
+const raw = document.querySelector(".main_raw");
+const toogle = document.querySelector(".toogle_preview");
+
 function setContent(page) {
-  const main = document.querySelector(".main_content");
-  if (!main) {
+  if (!html | !raw) {
     console.error("Main content element not found");
     return;
   }
 
   fetch(`pages/${page}.html`)
     .then((res) => res.text())
-    .then((html) => {
-      main.innerHTML = html;
+    .then((content) => {
+      html.innerHTML = content;
+      raw.textContent = content;
     });
 }
 
+function tooglePreview() {
+  codePreview = !codePreview;
+  setPreview();
+}
+
+function setPreview() {
+  toogle.textContent = codePreview ? "👁️" : "</>";
+  html.className = codePreview ? "hidden" : "";
+  raw.className = codePreview ? "" : "hidden";
+}
+
 setContent("pages");
+setPreview();
